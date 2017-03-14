3faea4c93ad9bd37c000e90a6087a9036db5cc79
Ryan Porteous <rp10@hw.ac.uk>
2017-03-14 20:23:37 +0000

    Almost complete view-job
    view-job almost complete. Still needs auto resize textareas.
    
    Still need to implement searching functionality of job-search

--- view-job.php
+++ view-job.php
@@ -36,13 +36,7 @@
 						<div class="col-md-5 col-sm-5">
 							<div class="header">
 								<div class="header-logo">
-									<!-- -->
-									<!-- -->
-									<!-- Can fill this name with actual job name-->
-									<!-- -->
-									<!-- -->
-									<!-- -->
-									<h1>Project Name</h1>
+									<h1>View Job</h1>
 								</div>
 							</div>
 						</div>
@@ -71,88 +65,133 @@
 										<a href="#employee-view" aria-controls="employee-view" role="tab" data-toggle="tab">Employee View</a>
 									</li>
 								</ul>
-
 								<!-- Tab panes -->
 								<div class="tab-content">
 									<div role="tabpanel" class="tab-pane active" id="employee-view">
 										<div class="employee-profile-details">
-											<div class="single-employee-profile">
-												<div class="user-profile-details">
-													<?php
+											<?php	
+												echo "<div class='single-employee-profile'>";
+													echo "<div class='user-profile-details'>";
 														
-														$jobID = $_POST["jobID"];
+															$jobID = $_POST["jobID"];
+															
+															$servername = "localhost";
+															$username = "root";
+															$password = "root";
+															$database = "jrg2";
+															
+															$conn = new mysqli($servername, $username, $password, $database);
+															
+															//Check connection
+															if($conn->connect_error) {
+																die("Connection to MySQL failed %s </br>" . $conn->connect_error);
+															}
+															
+															$sql1 = "SELECT * FROM job WHERE jobID = " . $jobID;
+															$result1 = $conn->query($sql1);
+															
+															$sql2 = "SELECT city, country FROM projects AS p, job AS j 
+																WHERE p.projID = j.projID AND j.jobID =" . $jobID;
+															$result2 = $conn->query($sql2);
+															
+															$sql3 = "SELECT skillName, yearsOfXP FROM job AS j, jobskills AS s 
+															WHERE s.jobID = j.jobID AND j.jobID =" . $jobID;
+															$result3 = $conn->query($sql3);
+															
+															//if there are results of every query
+															if($result1->num_rows > 0 && $result2->num_rows > 0 
+																&& $result3->num_rows >0) {
+															
+																$row1 = $result1->fetch_assoc();
+																$row2 = $result2->fetch_assoc();
+																
+																//Contains the details of the job passed by previous page
+																echo "<div class='users-profiles project-name'>";
+																	echo "<div class='single-users-profile'>";
+																		echo "<label for='jobTitle'>Job Title:</label>";
+																		echo "<input class='jobNames' id='jobTitle' name='job-title' type='text' 
+																			value='" . $row1['title'] ."' readonly='true'>";
+																	echo "</div>";
+																	echo "<div class='single-users-profile'>";
+																		echo "<label for='startDate'>Start Date:</label>";
+																		echo "<input class='sdates' id='startDate' name='start-date' type='text'
+																			value='" . $row1['startDate'] . "' readonly='true'>";
+																	echo "</div>";
+																	echo "<div class='single-users-profile'>";
+																		echo "<label for='endDate'>End Date:</label>";
+																		echo "<input class='sdates' id='endDate' name='end-date' type='text'
+																			value='" . $row1['endDate'] . "' readonly='true'>";
+																	echo "</div>";
+																	echo "<div class='single-users-profile'>";
+																		echo "<label for='jobLocation'>Job Location:</label>";
+																		echo "<input class='locations' id='jobLocation' name='location' type='text'
+																			value='" . $row2['city'] . "' readonly='true'>";
+																		echo "<input class='locations' id='jobLocation' name='location' type='text'
+																			value='" . $row2['country'] . "' readonly='true'>";
+																	echo "</div>";																
+																	echo "<div class='single-users-profile'>";
+																		echo "<label for='contractType'>Contract Type:</label>";
+																		echo "<input class='cTypes' id='contractType' name='contract-type' type='text'
+																			value='" . $row1['contractType'] . "' readonly='true'>";
+																	echo "</div>";
+																	echo "<div class='single-users-profile'>";
+																		echo "<label for='salary'>Salary:</label>";
+																		echo "<input class='salary' id='salary' name='salary' type='text'
+																			value='" . $row1['salary'] . "' readonly='true'>";
+																	echo "</div>";																
+																	echo "<div class='user-skill-details'>";
+																		echo "<label for='details'>Details:</label>";																		
+																		echo "<textarea class='details' id='details' name='details' readonly='true'>"
+																			 . $row1['details'] . "</textarea>";
+																	echo "</div>";
+																																		
+																	echo "<div class='user-skill-details'>";
+																		echo "<table align='center'>";
+																			echo "<tr>";
+																				echo "<th>Skill Required</th>";
+																				echo "<th>Years of Experience Required</th>";
+																			echo "</tr>";
+																		
+																			while($row3 = $result3->fetch_assoc()) {																			
+																				echo "<tr>";
+																					echo "<td>" . $row3['skillName'] . "</td>";
+																					echo "<td>" . $row3['yearsOfXP'] . "</td>";
+																				echo "</tr>";
+																			}			
+
+																		echo "</table>";																			
+																	echo "</div>";												
+																echo "</div>";
+																
+																//Button allows user to apply to project
+																//This needs to be changed
+																echo "<div class='apply-button'>";
+																	echo "<button type='submit'>Apply to Job</button>";
+																echo "</div>";
+															
+															} else {
+																echo "Something went wrong.</br></br></br>";
+															}
 														
-														$servername = "localhost";
-														$username = "root";
-														$password = "root";
-														$database = "jrg2";
-														
-														$conn = new mysqli($servername, $username, $password, $database);
-														
-														//Check connection
-														if($conn->connect_error) {
-															die("Connection to MySQL failed %s </br>" . $conn->connect_error);
-														}
+													echo "</div>";
 													
-														//Contains the details of the job passed by previous page
-														echo "<div class='users-profiles project-name'>";
-															echo "<div class='single-users-profile'>";
-																echo "<label for='jobTitle'>Job Title:</label>";
-																echo "<input class='jobNames' id='jobTitle' name='job-title' type='text'>";
+													//info about current project text
+													echo "<div class='user-profile-skill'>";	
+														echo "<div class='two-button'>";
+															echo "<div class='submit-change'>";
+																echo "<div class='return-button'>";
+																	echo "<a href='job-search.php'>Return to Job Search</a>";
+																echo "</div>";
 															echo "</div>";
-															echo "<div class='single-users-profile'>";
-																echo "<label for='startDate'>Start Date:</label>";
-																echo "<input class='sdates' id='sdate' name='start-date' type='text'>";
-															echo "</div>";
-															echo "<div class='single-users-profile'>";
-																echo "<label for='startDate'>Start Date:</label>";
-																echo "<input class='sdates' id='sdate' name='start-date' type='text'>";
-															echo "</div>";
-															echo "<div class='single-users-profile'>";
-																echo "<label for='jobLocation'>Job Location:</label>";
-																echo "<input class='locations' id='jobLocation' name='location' type='text'>";
+															echo "<div class='return-employee'>";
+																echo "<div class='return-button'>";
+																	echo "<a href='employeeMenu.php'>Return to Main Menu</a>";
+																echo "</div>";
 															echo "</div>";
 														echo "</div>";
-													
-														echo "<div class='users-profiles project-name'>";
-															echo "<div class='single-users-profile'>";
-																echo "<label for='ptype'>Contract Type:</label>";
-																echo "<input class='cTypes' id='contractType' name='contract-type' type='text'>";
-															echo "</div>";
-															echo "<div class='single-users-profile'>";
-																echo "<label for='startdate'>End Date:</label>";
-																echo "<input class='edates' id='endDate' name='end-date' type='text'>";
-															echo "</div>";
-														echo "</div>";							
-													?>
-												</div>
-												
-												<!-- info about current project text-->
-												<div class="user-profile-skill">
-													<div class="user-skill-details">
-														<textarea class="uskill" id="skills" name="skill" placeholder="This area will contain informaiton regarding the current project."></textarea>
-													</div>
-													
-													<!--Button allows user to apply to project
-														This needs to be changed -->
-													<div class='apply-button'>
-														<button type='submit'>Apply to Project</button>
-													</div>
-															
-													<div class="two-button">
-														<div class="submit-change">
-															<div class="return-button">
-																<a href="project-search.html">Return to Project Search</a>
-															</div>
-														</div>
-														<div class="return-employee">
-															<div class="return-button">
-																<a href="employee-menu.html">Return to Main Menu</a>
-															</div>
-														</div>
-													</div>
-												</div>                                            
-											</div>
+													echo "</div>";                                           
+												echo "</div>";
+											?>
 										</div>
 									</div>
 								</div>
