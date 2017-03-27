f1049a39cf0fbc8e444e9e0ffd8f1570cac4740b
Ryan Porteous <rp10@hw.ac.uk>
2017-03-27 21:17:06 +0100

    Created Folder for new pages and plan
    Added new pages to newPages folder and plan to plan folder

---
+++ newPages/editedProfile_db.php
@@ -0,0 +1,45 @@
+<?php
+	session_start();
+
+	if (!isset($_SESSION['username'])) {
+	  header('Location: projectLogin.php');
+	  exit; 
+	}
+	
+$servername = "localhost";      //connections set up
+$connUsername = "jrg2";
+$password = "password";
+$dbname = "jrg2";
+
+$username = $_SESSION["username"];
+
+//$username=$_POST['username'];
+$firstName=$_POST['first-name'];
+$lastName=$_POST['last-name'];
+$emailAddress=$_POST['email-address'];
+$city=$_POST['location1'];
+$country=$_POST['location2'];
+$contactNumber=$_POST['contact-number'];
+$additionalInfo=$_POST['additional-informaiton'];
+
+
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $connUsername, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    //Update the data regardless if it has changed or not
+    $sql = "UPDATE employee SET firstName = '$firstName', lastName = '$lastName', emailAddress = '$emailAddress', contactNumber = '$contactNumber', city = '$city', country = '$country', additionalInfo = '$additionalInfo' WHERE username = '$username'";
+
+    $conn->exec($sql);
+   
+
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+	echo "Profile Updated";
+	echo '<a href="your-profile.php">Click here</a>';
+?>
---
+++ newPages/storedPass_db.php
@@ -0,0 +1,33 @@
+<?php
+	session_start();
+
+	$username=$_POST['username'];
+	$userpass=$_POST['passwd'];
+	$con=mysqli_connect("localhost","jrg2","password","jrg2");
+      
+	// Check connection
+	if (mysqli_connect_errno())
+    {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+
+	//Run the query, store result in a string and store number of rows returned 
+    $result= mysqli_query($con,"SELECT position FROM accdetails WHERE username='$username' AND passwd='$userpass'") or die("Error: ".mysqli_error($con));
+  
+     
+	//Return the number of rows in result set
+	$rowcount=mysqli_num_rows($result);
+    if($rowcount !=0)
+	{
+		echo("Result set has $rowcount rows<br>");
+		while ($row = mysqli_fetch_assoc($result))
+		{
+			$_SESSION["username"] = $username;
+			header("Location:menu.php");
+		}
+	} else
+	{
+		header("Location:incorrect_password.php"); #failed login gets error message and another chance to login
+	}
+?>
+
