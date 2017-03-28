0a482158ee8ea67faa49304311d6fd67be41dbf4
Ryan Porteous <rp10@hw.ac.uk>
2017-03-28 19:37:56 +0100

    Removed willingToRelocate from edit-profile

--- newPages/edit-profile.php
+++ newPages/edit-profile.php
@@ -141,7 +141,7 @@
 			}
 
 			# Run the query, store result in a string and store number of rows returned 
-			$result= mysqli_query($con,"SELECT `firstName`, `lastName`, `emailAddress`, `contactNumber`,`city`,`country`,`additionalInfo`, `willingToRelocate`  FROM employee WHERE username='$username'") or die("Error: ".mysqli_error($con));
+			$result= mysqli_query($con,"SELECT `firstName`, `lastName`, `emailAddress`, `contactNumber`,`city`,`country`,`additionalInfo` FROM employee WHERE username='$username'") or die("Error: ".mysqli_error($con));
 		  
 			 
 			// Return the number of rows in result set
@@ -193,19 +193,7 @@
 																			<div class="single-users-profile">
 																				<label for="cnumber">Contact Number:</label>
 																				<input class="cnumbers" id="cnumber" name="contact-number" type="text" value="<?php echo $row['contactNumber']?>">
-																			</div>
-																			<div class="user-radio-button">
-																				<label for="wrlocate">Willing to relocate</label>													
-																				<?php																			
-																					if(strcasecmp($row['willingToRelocate'], "no") == 0) {																					
-																						echo "<input type='radio' id='wrlocate' name='gender' value='yes'> Yes";
-																						echo "<input type='radio' id='wrlocate' name='gender' value='no' checked> No";
-																					} else {
-																						echo "<input type='radio' id='wrlocate' name='gender' value='yes' checked> Yes";
-																						echo "<input type='radio' id='wrlocate' name='gender' value='no'> No";
-																					}
-																				?>
-																			</div>
+																			</div>																			>
 																		</div>
 																	</div>
 																	<div class="user-profile-skrill">	
